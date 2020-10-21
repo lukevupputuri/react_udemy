5d5067ada38448097def2918196f60eebfcbbb8e
Lazy9102 <vupputurilukevikas@gmail.com>
2020-10-21 17:29:53 -0400

    jsx

---
+++ jsx/src/index.js
@@ -0,0 +1,25 @@
+// Import the REact and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Create a react component
+const App = () => {
+    const buttonText = 'Click Me!';
+    return <div> 
+       <label className="label" for ="name">
+           Enter Name:
+           </label>
+           <input id="name" type="text" />
+<button style={{backgroundColor : 'blue', color: 'white'}}>
+    {buttonText}
+    </button>
+        </div>
+};
+
+
+// Take the react component and show it on the screen
+
+ReactDOM.render(
+<App />,
+document.querySelector('#root')
+);
